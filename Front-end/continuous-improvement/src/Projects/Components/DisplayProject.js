1c48439821502c32e6ef4f4cc048cf78d10833ef
Ulwothar <mateuszkrp@gmail.com>
2020-10-07 13:04:40 +0100

    Changed select inputs in New Suggestion form to react-select and made it working. Still looking for a way to make it required and I'll probably need to make a custom hook for that purpose, and this hook will be used to validate all of my form inputs.

--- Front-end/continuous-improvement/src/Projects/Components/DisplayProject.js
+++ Front-end/continuous-improvement/src/Projects/Components/DisplayProject.js
@@ -1,41 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
 
+import { statusSelect } from '../../shared/UIComponents/form-select';
 import './DisplayProject.css';
 
 const DisplayProject = (props) => {
+  const [value, setValue] = useState(props.status);
+
+  const statusOptions = statusSelect();
   return (
     <div className="project-window">
       <h3 className="suggestion-title">{props.title}</h3>
       <p className="suggestion-date">{props.date}</p>
-      <span className="suggestion-details">
+      <form className="modify-project-form">
+        <span className="suggestion-details">
+          <label>
+            Name:
+            <p>{props.name}</p>
+          </label>
+          <label>
+            Department:
+            <p>{props.department}</p>
+          </label>
+          <label>
+            Shift:
+            <p>{props.shift}</p>
+          </label>
+          <label>
+            Type:
+            <p>{props.type}</p>
+          </label>
+        </span>
         <label>
-          Name:
-          <p>{props.name}</p>
+          Status:
+          <Select
+            className="status-select"
+            options={statusOptions}
+            defaultValue={{ label: value, value: value }}></Select>
         </label>
-        <label>
-          Department:
-          <p>{props.department}</p>
-        </label>
-        <label>
-          Shift:
-          <p>{props.shift}</p>
-        </label>
-        <label>
-          Type:
-          <p>{props.type}</p>
-        </label>
-      </span>
-      <label>
-        Status:<p>{props.status}</p>
-      </label>
-      <div className="suggestion-description">
-        <h4 className="description-label">Current situation:</h4>
-        <p className="description-text">{props.currentSituation}</p>
-        <h4 className="description-label">Improvement suggestion:</h4>
-        <p className="description-text">{props.improvementSuggestion}</p>
-        <h4 className="description-label">Comments:</h4>
-        <p className="description-text">{props.comments}</p>
-      </div>
+        <div className="suggestion-description">
+          <h4 className="description-label">Current situation:</h4>
+          <p className="description-text">{props.currentSituation}</p>
+          <h4 className="description-label">Improvement suggestion:</h4>
+          <p className="description-text">{props.improvementSuggestion}</p>
+          <h4 className="description-label">Comments:</h4>
+          <p className="description-text">{props.comments}</p>
+        </div>
+      </form>
     </div>
   );
 };
