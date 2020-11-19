d5d2be66fdeb9cc448a876246951f86e8aeedb55
Ulwothar <mateuszkrp@gmail.com>
2020-11-19 01:40:24 +0000

    User have access to API (projects by Id for now only) with access token and refresh token. When first access token expires and all neccessary data is correct (user login and refresh token), user gets a new access token and is still able to view data from API. The problem is however that new access token comes in already expired and every time the very same access token is generated, so my next ToDo is to fix this issue and when it's fixed, implement token checking with every API call

--- Back-end/controllers/tokens-controller.js
+++ Back-end/controllers/tokens-controller.js
@@ -3,33 +3,50 @@ import Token from '../models/token';
 import HttpError from '../models/http-error';
 
 function genereateToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
 }
 
 export const CheckToken = async (token) => {
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
-    if (err) return false;
-    else return true;
-  });
+  try {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 };
 
 export const RefreshAccessToken = async (token, user) => {
   if (token == null) return null;
+  let accessToken;
   try {
     const refreshToken = await Token.findOne({
       refreshToken: token,
-      name: user,
+      name: user.login,
     });
-    if (!refreshToken) return null;
+    if (!refreshToken) {
+      console.log('Refresh token does not exist!');
+      return null;
+    }
+    //console.log({ refreshToken: refreshToken.refreshToken });   //Checking if I'm getting correct info from DB
 
     jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-      if (err) return err;
-      const accessToken = genereateToken(user);
-      return accessToken;
+      if (err) {
+        console.log('Something went wrong, could not verify your tokens!');
+        return err;
+      }
+      accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '30000',
+      });
+
+      //console.log(accessToken);     //Checking if I'm correctly getting to this point and generating new access token
+      //return genereateToken(user);
     });
   } catch (err) {
     return err;
   }
+
+  return accessToken;
 };
 
 export const DeleteToken = async (token, user) => {
