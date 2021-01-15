a7349d35877ab067f1f739f6b9756a5d90775e2d
Ulwothar <mateuszkrp@gmail.com>
2021-01-15 00:52:05 +0000

    Changed styling on page 'Projects' to make it more in line with the rest of the app.

--- Front-end/src/Projects/Pages/AllProjects.js
+++ Front-end/src/Projects/Pages/AllProjects.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import LoadAll from '../Components/LoadAll';
-import LoadProjects from '../Components/LoadProjects';
 
 const AllProjects = () => {
   return <LoadAll />;
